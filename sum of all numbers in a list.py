1ffd63b8377a1ffef152585bc5eb0385602f978e
anandasai sunke <98510557+SunkeAnandasai3784@users.noreply.github.com>
2022-11-25 19:35:14 +0530

    A Python function to sum all the numbers in a list.
    Note:- Sample List : (8, 2, 3, 0, 7)
    Expected Output : 20
    Explanation:
    Summation should like 8+2+3+0+7 = 20

---
+++ sum of all numbers in a list.py
@@ -0,0 +1,7 @@
+def sum(game_numbers):
+    total = 0
+    for i in game_numbers:
+        total += i
+    return total
+print(sum((8, 2, 3, 0, 7)))#excepted outcome is taken by built without user method.
+print("Summation should like 8+2+3+0+7 = 20")
